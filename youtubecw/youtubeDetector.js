ef354ea0de6b0e026e5b7e806322af428452d170
Hidde IJzerman <hiddeijzerman@gmail.com>
2018-02-26 14:48:41 +0100

    Small adjustments, tab detection (not very pretty)

--- youtubecw/youtubeDetector.js
+++ youtubecw/youtubeDetector.js
@@ -4,22 +4,35 @@ $( document ).ready(function() {
     var $path   = window.location.pathname;
     var $url    = window.location.href;
     var $option = false;
+    var $defurl = "https://www.youtube.com/";
 
-    // Get all tab URLs
+    // init test
+    getWindows();
+
     function getWindows() {
         chrome.windows.getAll({populate: true}, function (windows) {
             windows.forEach(function (window) {
                 window.tabs.forEach(function (tab) {
-                    //collect all of the urls here, I will just log them instead
-                    console.log(tab.url);
+                    // to-do: loop through and save tab containing YouTube
+                    // to-do: store tab containing YouTube so we can refer back to it at a later point
+
+                    var $url = tab.url;
+                    var $temp = $url.split("/");
+                    var $baseUrl = $temp[0] + "//" + $temp[2];
+                    console.log($baseUrl);
+
+                    if($baseUrl === "https://www.youtube.com") {
+                        console.error("YouTube detected on tab " + tab.id + ". (" + tab.url + ")");
+                    }
                 });
             });
-
-            warnUserClosure($option);
         });
     }
 
+
+
     function warnUserClosure($option) {
+        // just a test
         switch ($option) {
             case true: {
                 window.alert("Case TRUE");
@@ -32,13 +45,19 @@ $( document ).ready(function() {
     }
 
     function sendModalWindow($msg) {
+        // default js modal garbage
         var $confirmModal = confirm($msg);
 
-        if ($confirmModal == true) {
+        if ($confirmModal === true) {
             closeBrowser();
         } else {
             return false;
         }
+    }
+
+    function checkTabClosedDest($origin) {
+        // to-do: ¯\_(ツ)_/¯
+        return false;
     }
 
     function closeBrowser() {
